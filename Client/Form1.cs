22bd778845c6af4286f58eb85d74c1cf978d9a7e
Mattia <locastromattia@hotmail.it>
2019-07-11 12:54:08 +0200

    Communication TCP working

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -22,18 +22,12 @@ namespace Client
         {
             InitializeComponent();
             
-            try
-            {
-                client = new TcpClient(server_addr, server_port);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Client Istance Exception : " + ex.ToString());
-            }
         }
 
         private void btn_test_Click(object sender, EventArgs e)
         {
+            client = new TcpClient(server_addr, server_port);
+
             int msgLenght = Encoding.ASCII.GetByteCount(tb_test.Text);  //lunghezza in byte del messaggio
             byte[] msg_data = new byte[msgLenght];                      //inzializzo array con dim = lunghezza del messaggio
             msg_data = Encoding.ASCII.GetBytes(tb_test.Text);           //inserisco nell array il messaggio in byte
@@ -41,6 +35,7 @@ namespace Client
             NetworkStream stream = client.GetStream();
             stream.Write(msg_data, 0, msgLenght);
             stream.Close();
+            client.Close();
         }
     }
 }
--- Server/Server.cs
+++ Server/Server.cs
@@ -12,72 +12,46 @@ namespace Server
     {
         public Server()
         {
-            TcpListener server = null;
+            int port = 7777;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener (ip, port);
+            TcpClient client = default(TcpClient);
+
             try
             {
-                // Settiamo tcpListener con indirizzo e porta 7777.
-                Int32 port = 7777;
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-                
-                server = new TcpListener(localAddr, port);  // TcpListener server = new TcpListener(port);
-
-                server.Start();  // Start server per richieste clietn.
-
-                // Buffer for reading data
-                Byte[] bytes = new Byte[256];
-                String data = null;
-
-                // Loop di ascolto
-                while (true)
-                {
-                    
-                    Console.Write("Socket-->"+ " Indirizzo: "+localAddr+ " Port: "+ port + "\n\nWaiting for a connection... ");
-
-                    // Perform a blocking call to accept requests.
-                    TcpClient client = server.AcceptTcpClient();    //Si accetta la richiesta del client
-                    Console.WriteLine("Connected!");
-
-                    data = null;
-
-                    NetworkStream stream = client.GetStream();  //Oggeto stream per leggere e scrivere
-
-                    int i;
-                    // Loop per ricevere tutti i dati inviati dal client
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0) //Se ricevo qualcosa leggiamo il messaggio
-                    {
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i); //Conversione da byte a ASCII
-                        Console.WriteLine("Received: {0}", data);
-
-                        
-                        data = data.ToUpper();  // stringa in Upper case
-
-                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                        // Ritorna un messaggio di conferma al client
-                        stream.Write(msg, 0, msg.Length);
-                        Console.WriteLine("Sent: {0}", data);
-                    }
-
-                    // Shutdown and end connection
-                    client.Close();
-                }
+                server.Start();
+                Console.WriteLine("Server started");
+                     
 
             }
             catch (SocketException e)
             {
                 Console.WriteLine("SocketException: {0}", e);
+                Console.Read();
             }
-            finally
+
+            while (true)
             {
-                // Stop listening for new clients.
-                server.Stop();
+                client = server.AcceptTcpClient();
+
+                byte[] buffer = new byte[256];
+                NetworkStream stream = client.GetStream();
+
+                stream.Read(buffer, 0, buffer.Length);
+
+                StringBuilder msg = new StringBuilder();
+
+                foreach(byte b in buffer)
+                {
+                    if (b.Equals(00))
+                        break;
+                    else
+                        msg.Append(Convert.ToChar(b).ToString());
+
+                }
+
+                Console.WriteLine(msg+" "+buffer.Length);
             }
-
-
-            Console.WriteLine("\nHit enter to continue...");
-            Console.Read();
-
         }
 
     }
