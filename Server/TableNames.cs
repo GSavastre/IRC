dd5ae0ed23d6e614cad8ea8af460c314858dd338
Gabriele <savastrecosmingabriele@gmail.com>
2019-07-12 12:39:37 +0200

    Added DBManager class and TableNames class

---
+++ Server/TableNames.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server {
+    /// <summary>
+    /// Nomi delle tabelle DB.
+    /// </summary>
+    static class TableNames {
+        public const string messagesTable = "messages";
+        public const string serversTable = "servers";
+        public const string usersTable = "users";
+        public const string groupTable = "groups";
+    }
+}
