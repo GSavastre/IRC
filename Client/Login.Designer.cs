1db4b8610531cc1d3f749605f70fefc315c15e07
Mattia <locastromattia@hotmail.it>
2019-07-12 18:58:07 +0200

    Login and Register form + correct switch process form

---
+++ Client/Login.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Client
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_switch_reg = new System.Windows.Forms.Button();
+            this.tb_log_username = new System.Windows.Forms.TextBox();
+            this.tb_log_password = new System.Windows.Forms.TextBox();
+            this.lb_username = new System.Windows.Forms.Label();
+            this.lb_password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(166, 166);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(108, 51);
+            this.btn_login.TabIndex = 0;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // btn_switch_reg
+            // 
+            this.btn_switch_reg.Location = new System.Drawing.Point(319, 222);
+            this.btn_switch_reg.Name = "btn_switch_reg";
+            this.btn_switch_reg.Size = new System.Drawing.Size(83, 23);
+            this.btn_switch_reg.TabIndex = 1;
+            this.btn_switch_reg.Text = "Registrazione";
+            this.btn_switch_reg.UseVisualStyleBackColor = true;
+            this.btn_switch_reg.Click += new System.EventHandler(this.btn_switch_reg_Click);
+            // 
+            // tb_log_username
+            // 
+            this.tb_log_username.Location = new System.Drawing.Point(134, 46);
+            this.tb_log_username.Name = "tb_log_username";
+            this.tb_log_username.Size = new System.Drawing.Size(240, 20);
+            this.tb_log_username.TabIndex = 2;
+            // 
+            // tb_log_password
+            // 
+            this.tb_log_password.Location = new System.Drawing.Point(134, 111);
+            this.tb_log_password.Name = "tb_log_password";
+            this.tb_log_password.Size = new System.Drawing.Size(240, 20);
+            this.tb_log_password.TabIndex = 3;
+            // 
+            // lb_username
+            // 
+            this.lb_username.AutoSize = true;
+            this.lb_username.Location = new System.Drawing.Point(43, 53);
+            this.lb_username.Name = "lb_username";
+            this.lb_username.Size = new System.Drawing.Size(55, 13);
+            this.lb_username.TabIndex = 4;
+            this.lb_username.Text = "Username";
+            // 
+            // lb_password
+            // 
+            this.lb_password.AutoSize = true;
+            this.lb_password.Location = new System.Drawing.Point(45, 114);
+            this.lb_password.Name = "lb_password";
+            this.lb_password.Size = new System.Drawing.Size(53, 13);
+            this.lb_password.TabIndex = 5;
+            this.lb_password.Text = "Password";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 255);
+            this.Controls.Add(this.lb_password);
+            this.Controls.Add(this.lb_username);
+            this.Controls.Add(this.tb_log_password);
+            this.Controls.Add(this.tb_log_username);
+            this.Controls.Add(this.btn_switch_reg);
+            this.Controls.Add(this.btn_login);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_switch_reg;
+        private System.Windows.Forms.TextBox tb_log_username;
+        private System.Windows.Forms.TextBox tb_log_password;
+        private System.Windows.Forms.Label lb_username;
+        private System.Windows.Forms.Label lb_password;
+    }
+}
---
+++ Client/Login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public partial class Login : Form
+    {
+        string server_addr = "127.0.0.1";
+        int server_port = 7777;
+        TcpClient client;
+
+        public Login()
+        {
+            InitializeComponent();
+            this.DialogResult = DialogResult.OK; ;
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient(server_addr, server_port);
+
+            Message regMessage = new Message(tb_log_username.Text, tb_log_password.Text, 0); //oggetto messagge per registrazione action = 0
+
+            int msgLenght = Encoding.ASCII.GetByteCount(tb_log_password.Text);  //lunghezza in byte del messaggio
+            byte[] msg_data = new byte[msgLenght];                      //inzializzo array con dim = lunghezza del messaggio
+            msg_data = Encoding.ASCII.GetBytes(tb_log_password.Text);           //inserisco nell array il messaggio in byte
+
+            NetworkStream stream = client.GetStream();
+            stream.Write(msg_data, 0, msgLenght);
+            stream.Close();
+            client.Close();
+        }
+
+        private void btn_switch_reg_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Yes;
+            this.Close();
+        }
+    }
+}
