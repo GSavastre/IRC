1ce999fa221c8aea879b6d6b5c181ebb91cfa46f
Mattia <locastromattia@hotmail.it>
2019-07-12 11:59:43 +0200

    Class Message

---
+++ Server/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+    class Message
+    {
+        public IPAddress ipSender;
+        public int portSender;
+        public IPAddress ipReciver;
+        public int portReciver;
+        public string message;
+        public int hashCode; //TODO
+
+        public Message(IPAddress myIpSender, int myPortSender, IPAddress myIpReciver, int myPortReciver, string myMessage)
+        {
+            ipSender = myIpSender;
+            portSender = myPortSender;
+            ipReciver = myIpReciver;
+            portReciver = myPortReciver;
+            message = myMessage;
+        }
+
+    }
+}
