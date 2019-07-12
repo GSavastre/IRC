96794d70a5f4e8f80ec6343987677f6d19559936
Cosmin <cosmindaxgugoasa@gmail.com>
2019-07-12 11:54:27 +0200

    minor fixes

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Net;
 using System.Net.Sockets;
 
 namespace Client
@@ -21,7 +14,6 @@ namespace Client
         public Form1()
         {
             InitializeComponent();
-            
         }
 
         private void btn_test_Click(object sender, EventArgs e)
