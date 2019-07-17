7abb5c78a83210ca5566bca9019831dcbc2e8a73
Gabriele <savastrecosmingabriele@gmail.com>
2019-07-17 13:11:47 +0200

    Additional work done yup yup

--- Client/Login.cs
+++ Client/Login.cs
@@ -53,47 +53,47 @@ namespace Client
                     while (true)
                     {
                         TcpClient clientlistener;
-                        clientlistener = listener.AcceptTcpClient();
-                        byte[] buffer = new byte[1024];
-                        NetworkStream streamlistener = clientlistener.GetStream();
-                        int len = streamlistener.Read(buffer, 0, buffer.Length);
-    
-                        if (((List<ircUser>)ircMessage.BytesToObj(buffer, len)).Count() == 0)
-                        {
-                            MessageBox.Show("Invalid Login !");
-                            streamlistener.Close();
-                            clientlistener.Close();
-                            break;
-                        }
-                        else
-                        {
+                        try {
+                            byte[] buffer = new byte[1024];
+                            clientlistener = listener.AcceptTcpClient();
+                            NetworkStream streamlistener = clientlistener.GetStream();
+                            int len = streamlistener.Read(buffer, 0, buffer.Length);
 
-                            listener.Stop();
-                            pingServerThread.Start();
-                            online_users = (List<ircUser>)ircMessage.BytesToObj(buffer, len);
-                                
-                            streamlistener.Close();
-                            clientlistener.Close();
+                            if (((List<ircUser>)ircMessage.BytesToObj(buffer, len)).Count() == 0) {
+                                MessageBox.Show("Invalid Login !");
+                                streamlistener.Close();
+                                clientlistener.Close();
+                                break;
+                            } else {
 
-                            Form home = new Home(server_addr, new ircUser(tb_log_username.Text, Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString()), online_users);
+                                listener.Stop();
+                                //pingServerThread.Start();
+                                online_users = (List<ircUser>)ircMessage.BytesToObj(buffer, len);
 
-                            this.Hide();
-                            home.ShowDialog();
+                                streamlistener.Close();
+                                clientlistener.Close();
+
+                                Form home = new Home(server_addr, new ircUser(tb_log_username.Text, Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString()), online_users);
+
+                                this.Hide();
+                                home.ShowDialog();
+                                this.Close();
+                                break;
+                            }
+                        } catch (Exception) {
+                            MessageBox.Show("Connessione al server scaduta, ritorno alla lista di server disponibili.", "Errore connessione al server");
                             this.Close();
                             break;
                         }
+                       
                     }
-                    
-
+                } catch{
+                    MessageBox.Show("Connessione al server scaduta, ritorno alla lista di server disponibili.","Errore connessione al server");
+                    this.Close();
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
+            } catch {
+                MessageBox.Show("Connessione al server scaduta, ritorno alla lista di server disponibili.", "Errore connessione al server");
+                this.Close();
             }
         }
 
