1956edf7c249de3f1260e428af892edb41eb720c
Mattia <locastromattia@hotmail.it>
2019-07-18 16:43:19 +0200

    fix closing home thread listener

--- Client/Home.cs
+++ Client/Home.cs
@@ -236,6 +236,24 @@ namespace Client
         {
             chatBox.AddMessage(msg.sender_username + " : " + msg.message);
         }
-        
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+
+            // Confirm user wants to close
+            switch (MessageBox.Show(this, "Are you sure you want to close?", "Closing", MessageBoxButtons.YesNo))
+            {
+                case DialogResult.No:
+                    e.Cancel = true;
+                    break;
+                default:
+                    listener.Stop();
+                    tcpListenerThread.Abort();
+                    break;
+            }
+        }
     }
 }
